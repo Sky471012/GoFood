04d74135dee473634826cb14dd10f1fd3eec3826
Sky471012 <sharma.aakash1012@gmail.com>
2025-03-13 14:34:39 +0530

    Daily update 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Home from './screens/Home'
 import Login from './screens/Login';
+import Signup from './screens/Signup';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-dark-5/dist/css/bootstrap-night.min.css";
@@ -18,6 +19,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home/>} />
           <Route exact path='/login' element={<Login/>} />
+          <Route exact path='/signup' element={<Signup/>} />
         </Routes>
       </div>
     </Router>
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Login() {
-  return (
-    <div>
-      Login
+
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/loginuser", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email: credentials.email, password: credentials.password })
+    });
+    const json = await response.json();
+    console.log(json);
+
+    if (!json.success) {
+      alert("Enter valid credentials!");
+    }else{
+      console.log("Logined");
+    }
+
+  }
+
+  const onChange = (event) => {
+    setCredentials({ ...credentials, [event.target.name]: event.target.value })
+  }
+
+  return (<>
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="m-3 form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' value={credentials.email} onChange={onChange} />
+        </div>
+        <div className="m-3 form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" name='password' value={credentials.password} onChange={onChange} />
+        </div>
+        <button type="submit" className="m-3 btn btn-primary">Login</button>
+        <Link to="/signup" className='m-3 btn btn-danger'>Not a user</Link>
+      </form>
     </div>
-  )
+  </>)
 }
