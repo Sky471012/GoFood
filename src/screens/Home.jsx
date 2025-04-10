f3d64220977834ee8262c66893147ad1df33454c
Sky471012 <sharma.aakash1012@gmail.com>
2025-04-10 18:00:39 +0530

    Cards are working ui

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,17 +1,107 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Card from '../components/Card'
-import Carousel from '../components/Carousel'
 
 export default function Home() {
+
+  const [search, setSearch] = useState('');
+  const [foodCat, setFoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
+
+  const loadData = async ()=>{
+    let response = await fetch("http://localhost:5000/api/foodData",{
+      method : "POST",
+      headers : {
+        'Content-Type' : 'application/json'
+      }
+    });
+
+    response = await response.json();
+
+    setFoodItem(response[0]);
+    setFoodCat(response[1]);
+  }
+
+  useEffect(()=>{
+    loadData();
+  }, [])
+
   return (
     <div>
       <Navbar/>
-      <Carousel/>
-      <Card/>
-      <Card/>
-      <Card/>
+      
+      {/* Carousel-starts */}
+      <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel" style={{objectFit: "contain !important"}}>
+            <div className="carousel-inner ">
+                <div className="carousel-caption" style={{zIndex:"10"}}>
+                    <div className="d-flex justify-content-center">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={search} onChange={(e)=>{setSearch(e.target.value)}} />
+                            {/* <button className="btn btn-outline-success text-white bg-success" type="submit">Search</button> */}
+                    </div>
+                </div>
+                <div className="carousel-item active">
+                    <img src="/burger.jpg" className="d-block w-100" style={{filter: "brightness(50%)", height:"100%", objectFit: "cover"}} alt="Burger" />
+                </div>
+                <div className="carousel-item">
+                    <img src="/pizza.jpg" className="d-block w-100" style={{filter: "brightness(50%)", height:"100%", objectFit: "cover"}} alt="Pizza" />
+                </div>
+                <div className="carousel-item">
+                    <img src="/momos.jpg" className="d-block w-100" style={{filter: "brightness(50%)", height:"100%", objectFit: "cover"}} alt="Momos" />
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+      {/* Carousel-ends */}
+
+
+      {/* Card-starts */}
+      <div className="container">
+      {
+        foodItem.length > 0 ? (
+          foodItem.map((data) => {
+            return (
+              <div className='row mb-3'>
+                <div key={data._id} className='fs-3 m-3'>
+                  {data.CategoryName}
+                </div>
+                <hr />
+                {
+                  foodItem.length > 0 ? (
+                    foodItem
+                      .filter((item) => (item.CategoryName === data.CategoryName) && (item.name.toLowerCase().includes(search.toLocaleLowerCase()))) 
+                      .map((filterItems) => (
+                        <div key={filterItems._id} className='col-12 col-md-6 col-lg-3'>
+                            <Card foodName={filterItems.name} 
+                                  options={filterItems.options}
+                                  imgSrc={filterItems.img}
+                                  foodDesc={filterItems.description}
+                            />
+                        </div>
+                      ))
+                  ) : (
+                    <div>No food item</div>
+                  )
+                }
+              </div>
+            );
+          })
+        ) : (
+          <div>No Food Category</div>
+        )
+      }
+
+      
+      
+      </div>
+      {/* Card-ends */}
       <Footer/>
     </div>
   )
